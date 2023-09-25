6f3da0af10355ad63aaaba08a753bb3941be823d
Ivan Shanygin <shanygin101@yandex.ru>
2023-09-25 22:07:46 +0300

    Added segment tree (max zeros in a row)

--- DataStructures/SegmentTree.cpp
+++ DataStructures/SegmentTree.cpp
@@ -176,6 +176,110 @@ void update_kth(std::vector<int> &tree, int v, int l, int r, int position, int n
 
 // }
 
+// { Макс кол-во подряд идущих нулей на подотрезке
+
+struct Node_ {
+    int middle;
+    int max_prefix;
+    int max_sufix;
+    bool all_zeros;
+};
+
+int n;
+std::vector<int> array;
+
+Node_ merge_nodes(Node_ &node1, Node_ &node2) {
+
+    Node_ node3 = {0, 0, 0, false};
+
+    if (node1.all_zeros)
+        node3.max_prefix = node1.max_prefix + node2.max_prefix;
+    else
+        node3.max_prefix = node1.max_prefix;
+
+
+    if (node2.all_zeros)
+        node3.max_sufix = node1.max_sufix + node2.max_prefix;
+    else
+        node3.max_sufix = node2.max_sufix;
+
+
+    node3.all_zeros = node1.all_zeros && node2.all_zeros;
+
+    node3.middle = std::max(node1.middle, std::max(node2.middle, node1.max_sufix + node2.max_prefix));
+
+    return node3;
+
+}
+
+void build_tree(std::vector<Node_> &tree, int v, int l, int r) {
+    if (l == r) {
+        if (array[l] == 0) {
+            tree[v].middle = 0;
+            tree[v].max_prefix = 1;
+            tree[v].max_sufix = 1;
+            tree[v].all_zeros = true;
+        }
+        else {
+            tree[v].middle = 0;
+            tree[v].max_prefix = 0;
+            tree[v].max_sufix = 0;
+            tree[v].all_zeros = false;
+        }
+    }
+    else {
+        int m = (l + r) / 2;
+        build_tree(tree, v * 2, l, m);
+        build_tree(tree, v * 2 + 1, m + 1, r);
+
+        tree[v] = merge_nodes(tree[2 * v], tree[2 * v + 1]);
+    }
+}
+
+void update(std::vector<Node_> &tree, int v, int l, int r, int position, int new_val) {
+    if (l == r) {
+        if (new_val == 0) {
+            tree[v].middle = 0;
+            tree[v].max_prefix = 1;
+            tree[v].max_sufix = 1;
+            tree[v].all_zeros = true;
+        }
+        else {
+            tree[v].middle = 0;
+            tree[v].max_prefix = 0;
+            tree[v].max_sufix = 0;
+            tree[v].all_zeros = false;
+        }
+    }
+    else {
+        int m = (l + r) / 2;
+        if (position <= m) {
+            update(tree, v * 2, l, m, position, new_val);
+        }
+        else {
+            update(tree, v * 2 + 1, m + 1, r, position, new_val);
+        }
+
+        tree[v] = merge_nodes(tree[2 * v], tree[2 * v + 1]);
+    }
+}
+
+Node_ getSegment(std::vector <Node_> &tree, int v, int l, int r, int req_l, int req_r) {
+    if (l == req_l && r == req_r) {
+        return tree[v];
+    }
+
+    int m = (l + r) / 2;
+    if (m >= req_r) return getSegment(tree, v * 2, l, m, req_l, std::min(req_r, m));
+    if (m < req_l) return getSegment(tree, v * 2 + 1, m + 1, r, std::max(req_l, m + 1), req_r);
+
+    Node_ lres = getSegment(tree, v * 2, l, m, req_l, m);
+    Node_ rres = getSegment(tree, v * 2 + 1, m + 1, r, m + 1, req_r);
+
+    return merge_nodes(lres, rres);
+}
+// }
+
 
 int main() {
     int n, req_l, req_r;
